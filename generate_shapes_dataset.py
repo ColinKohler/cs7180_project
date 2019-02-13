8bef02e05f96b60d265450c8fca7243f35ed8fc8
ColinKohler <colink78@gmail.com>
2019-02-13 18:36:36 -0500

    Some basic refactoring, added basic color and saving functionality, added debug flag.

--- generate_shapes_dataset.py
+++ generate_shapes_dataset.py
@@ -5,10 +5,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import skimage
 
-NUM_COLORS = 2
-
 def main(args):
-  # Dataset
+  # Init empty dataset
   samples = np.empty((args.num_samples, args.sample_size, args.sample_size, 3))
   labels = np.empty((args.num_samples, 1))
   queries = list()
@@ -27,21 +25,35 @@ def main(args):
     cells = np.dstack(np.meshgrid(cell_length, cell_length)).reshape(-1, 2)
 
     for cell_idx, mask in zip(cells, shape_masks):
-      # if mask generate shape randomly
-      # if npr.rand() > 0.5:
-      #   shape = generatePlus()
-      # else:
-      shape = generateRandomEllipse(pixel_cell_size)
+      # Check to see if we will generate a shape in this cell
+      if not mask:
+        continue
+
+      # Generate random shape of a random color
+      color_generators = [generateRandomRed, generateRandomGreen]
+      shape_generators = [generateRandomEllipse]
+
+      color_str, color_rgb = npr.choice(color_generators)()
+      shape = npr.choice(shape_generators)(pixel_cell_size, color_rgb)
 
       # Append shape into cell
       sample[cell_idx[1]*pixel_cell_size:(cell_idx[1]+1)*pixel_cell_size,
              cell_idx[0]*pixel_cell_size:(cell_idx[0]+1)*pixel_cell_size:, :] = shape
 
-
     # Generate and compare query to sample to get label
-    plt.imshow(sample, cmap='gray')
-    plt.show()
 
+    # Add sample to dataset
+    samples[i] = sample
+
+    if args.debug:
+      plt.imshow(sample)
+      plt.show()
+
+  # Save dataset
+  if not args.debug:
+    np.save('./datasets/simple_shapes/samples.npy', samples)
+    np.save('./datasets/simple_shapes/queries.npy', queries)
+    np.save('./datasets/simple_shapes/labels.npy', labels)
 
 def generateQuery():
   pass
@@ -49,26 +61,50 @@ def generateQuery():
 def getLabel(sample, query):
   pass
 
-def generatePlus(grid_cell, transform, color):
+def generateRandomPlus(cell_size, color_generators):
   ''' Generate plus in the desired grid_cell '''
   pass
 
-def generateRandomEllipse(cell_size):
+def generateRandomEllipse(cell_size, color):
   ''' Generate ellipse in the desired grid_cell '''
   default_radius = cell_size / 3
   center = cell_size / 2
-  color = None
 
-  cx = np.clip(center + int(npr.normal(0, cell_size/16)), cell_size * ((1/2) - (1/16)), cell_size * ((1/2) + (1/16)))
-  cy = np.clip(center + int(npr.normal(0, cell_size/16)), cell_size * ((1/2) - (1/16)), cell_size * ((1/2) + (1/16)))
-  r_major = np.clip(default_radius + int(npr.normal(0, cell_size/12)), cell_size * ((1/3) - (1/12)), cell_size * ((1/3) + (1/12)))
-  r_minor = np.clip(default_radius + int(npr.normal(0, cell_size/12)), cell_size * ((1/3) - (1/12)), cell_size * ((1/3) + (1/12)))
+  # Clip the center and radii so they fit in the cell
+  cx = np.clip(center + int(npr.normal(0, cell_size/16)),
+               cell_size * ((1/2) - (1/16)),
+               cell_size * ((1/2) + (1/16)))
+  cy = np.clip(center + int(npr.normal(0, cell_size/16)),
+               cell_size * ((1/2) - (1/16)),
+               cell_size * ((1/2) + (1/16)))
+  r_major = np.clip(default_radius + int(npr.normal(0, cell_size/12)),
+                    cell_size * ((1/3) - (1/12)),
+                    cell_size * ((1/3) + (1/12)))
+  r_minor = np.clip(default_radius + int(npr.normal(0, cell_size/12)),
+                    cell_size * ((1/3) - (1/12)),
+                    cell_size * ((1/3) + (1/12)))
 
+  # Draw the ellipse and set its color
   cell_img = np.zeros((cell_size, cell_size, 3))
   rr, cc = skimage.draw.ellipse(cx, cy, r_major, r_minor)
-  cell_img[rr,cc,:] = 1
+  cell_img[rr,cc,:] = color
 
   return cell_img
+
+# TODO: This should generate a random shade of red
+def generateRandomRed():
+  ''' Generates a random shade of red '''
+  return 'red', np.array([1., 0., 0.])
+
+# TODO: This should generate a random shade of green
+def generateRandomGreen():
+  ''' Generates a random shade of green '''
+  return 'green', np.array([0., 1., 0.])
+
+# TODO: This should generate a random shade of blue
+def generateRandomBlue():
+  ''' Generates a random shade of blue '''
+  return 'blue', np.array([0., 0., 1.])
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -78,6 +114,8 @@ if __name__ == '__main__':
       help='Size of the grid (default=2x2)')
   parser.add_argument('--sample_size', type=int, default=64,
       help='Sample size in pixels (64x64)')
+  parser.add_argument('--debug', default=False, action='store_true',
+      help='Enter debugging mode')
 
   args = parser.parse_args()
   main(args)
