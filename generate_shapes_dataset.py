fd777e8188f94ffe8f367b52c2f699d00c9bb48c
rsfwalters <rsfwalters@gmail.com>
2019-02-13 00:55:30 -0500

    added color; added antialiasing

--- generate_shapes_dataset.py
+++ generate_shapes_dataset.py
@@ -7,6 +7,7 @@ import skimage
 
 NUM_COLORS = 2
 
+
 def main(args):
   # Dataset
   samples = np.empty((args.num_samples, args.sample_size, args.sample_size, 3))
@@ -31,7 +32,7 @@ def main(args):
       # if npr.rand() > 0.5:
       #   shape = generatePlus()
       # else:
-      shape = generateRandomEllipse(pixel_cell_size)
+      shape = generateAntiAliasedRandomEllipse(pixel_cell_size)
 
       # Append shape into cell
       sample[cell_idx[1]*pixel_cell_size:(cell_idx[1]+1)*pixel_cell_size,
@@ -53,11 +54,29 @@ def generatePlus(grid_cell, transform, color):
   ''' Generate plus in the desired grid_cell '''
   pass
 
+def generateRandomColor():
+  ''' Generate one of NUM_COLORS colors plus some random noise'''
+  # Colors from HTML 4 sorted by commonness of names
+  colors = {"red":(1,0,0),"blue":(0,0,1),"green":(0,.5,0),"purple":(.5,0,.5),"yellow":(1,1,0),"aqua":(0,1,1),"orange":(1,0.65,0),"gray":(.5,.5,.5),"teal":(0,.5,.5),"navy":(0,0,.5),"lime":(0,1,0),"fuchsia":(1,0,1),"olive":(.5,.5,0),"maroon":(.5,0,0)}
+  # Truncate to NUM_COLORS
+  colors = dict(list(colors.items())[0:NUM_COLORS])
+  color_key_list = list(colors)
+  color_indx = npr.randint(0,NUM_COLORS)
+  center_RGB_color = list(colors[color_key_list[color_indx]])
+  noise = npr.normal(0, .15, 3)
+  return np.clip(center_RGB_color + noise,0,1)
+  
+def generateAntiAliasedRandomEllipse(cell_size):
+  ''' Generate anti aliased ellipse in the desired grid_cell '''
+  large_img = generateRandomEllipse(4*cell_size)
+  cell_img = skimage.transform.resize(large_img,(cell_size,cell_size))
+  return cell_img
+  
 def generateRandomEllipse(cell_size):
   ''' Generate ellipse in the desired grid_cell '''
   default_radius = cell_size / 3
   center = cell_size / 2
-  color = None
+  color = generateRandomColor()
 
   cx = np.clip(center + int(npr.normal(0, cell_size/16)), cell_size * ((1/2) - (1/16)), cell_size * ((1/2) + (1/16)))
   cy = np.clip(center + int(npr.normal(0, cell_size/16)), cell_size * ((1/2) - (1/16)), cell_size * ((1/2) + (1/16)))
@@ -66,8 +85,8 @@ def generateRandomEllipse(cell_size):
 
   cell_img = np.zeros((cell_size, cell_size, 3))
   rr, cc = skimage.draw.ellipse(cx, cy, r_major, r_minor)
-  cell_img[rr,cc,:] = 1
-
+  cell_img[rr,cc,:] = color
+  
   return cell_img
 
 if __name__ == '__main__':
