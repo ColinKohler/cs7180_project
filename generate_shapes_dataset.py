be13412ceb09db5eaab4e93d24f683165e5dc3c4
ColinKohler <colink78@gmail.com>
2019-02-14 10:33:26 -0500

    Added plus shape generation, refactored color and anti aliasing, added argument flag for using aa, and added string grid variable to use for generating the query/label.

--- generate_shapes_dataset.py
+++ generate_shapes_dataset.py
@@ -4,6 +4,7 @@ import numpy.random as npr
 import matplotlib
 import matplotlib.pyplot as plt
 import skimage
+import random
 
 def main(args):
   # Init empty dataset
@@ -22,25 +23,32 @@ def main(args):
     # Add shapes to sample
     shape_masks = npr.rand(num_cells) > 0.5
     cell_length = list(range(args.grid_size))
-    cells = np.dstack(np.meshgrid(cell_length, cell_length)).reshape(-1, 2)
+    cell_indexes = np.dstack(np.meshgrid(cell_length, cell_length)).reshape(-1, 2)
+    string_grid = np.empty((args.grid_size, args.grid_size), dtype=np.object)
 
-    for cell_idx, mask in zip(cells, shape_masks):
+    for cell_idx, mask in zip(cell_indexes, shape_masks):
       # Check to see if we will generate a shape in this cell
       if not mask:
         continue
 
       # Generate random shape of a random color
-      color_generators = [generateRandomRed, generateRandomGreen]
-      shape_generators = [generateRandomEllipse]
+      shape_generators = {'ellipse': generateRandomEllipse, 'plus': generateRandomPlus}
 
-      color_str, color_rgb = npr.choice(color_generators)()
-      shape = npr.choice(shape_generators)(pixel_cell_size, color_rgb)
+      color_str, color_rgb = generateRandomColor()
+      shape_str, shape_generator = random.choice(list(shape_generators.items()))
+      if args.aa:
+        shape = generateAntiAliasedShape(shape_generator, pixel_cell_size, color_rgb)
+      else:
+        shape = shape_generator(pixel_cell_size, color_rgb)
 
-      # Append shape into cell
+      # Append shape into cell and cell sting into string grid
       sample[cell_idx[1]*pixel_cell_size:(cell_idx[1]+1)*pixel_cell_size,
              cell_idx[0]*pixel_cell_size:(cell_idx[0]+1)*pixel_cell_size:, :] = shape
+      string_grid[cell_idx[1], cell_idx[0]] = '{}_{}'.format(color_str, shape_str)
 
     # Generate and compare query to sample to get label
+    query = generateQuery(string_grid)
+    label = getLabel(string_grid, query)
 
     # Add sample to dataset
     samples[i] = sample
@@ -55,20 +63,40 @@ def main(args):
     np.save('./datasets/simple_shapes/queries.npy', queries)
     np.save('./datasets/simple_shapes/labels.npy', labels)
 
-def generateQuery():
-  pass
+def generateQuery(grid):
+  return None
 
 def getLabel(sample, query):
-  pass
+  return None
 
-def generateRandomPlus(cell_size, color_generators):
-  ''' Generate plus in the desired grid_cell '''
-  pass
-
-def generateAntiAliasedRandomEllipse(cell_size):
+def generateAntiAliasedShape(shape_generator, cell_size, color):
   ''' Generate anti aliased ellipse in the desired grid_cell '''
-  large_img = generateRandomEllipse(4*cell_size)
-  cell_img = skimage.transform.resize(large_img,(cell_size,cell_size))
+  large_img = shape_generator(4*cell_size, color)
+  return skimage.transform.resize(large_img, (cell_size,cell_size), mode='constant', anti_aliasing=True)
+
+def generateRandomPlus(cell_size, color):
+  ''' Generate plus in the desired grid_cell '''
+  default_width = cell_size / 8
+  default_height = cell_size / 2
+  center = cell_size / 2
+
+  # Generate center, width, and height with noise
+  cx = int(center + int(npr.normal(0, cell_size/16)))
+  cy = int(center + int(npr.normal(0, cell_size/16)))
+  width = int(default_width + int(npr.normal(0, cell_size/24)))
+  height = int(default_height + int(npr.normal(0, cell_size/12)))
+
+  # Draw the plus and set its color
+  cell_img = np.zeros((cell_size, cell_size, 3))
+  rr1, cc1 = skimage.draw.rectangle([int(cx-height/2), int(cy-width/2)],
+                                    extent=[height, width],
+                                    shape=cell_img.shape[:2])
+  rr2, cc2 = skimage.draw.rectangle([int(cx-width/2), int(cy-height/2)],
+                                    extent=[width, height],
+                                    shape=cell_img.shape[:2])
+  cell_img[rr1,cc1,:] = color
+  cell_img[rr2,cc2,:] = color
+
   return cell_img
 
 def generateRandomEllipse(cell_size, color):
@@ -76,7 +104,7 @@ def generateRandomEllipse(cell_size, color):
   default_radius = cell_size / 3
   center = cell_size / 2
 
-  # Clip the center and radii so they fit in the cell
+  # Generate center and radii with noise. Clipping to ensure full shape is in cell
   cx = np.clip(center + int(npr.normal(0, cell_size/16)),
                cell_size * ((1/2) - (1/16)),
                cell_size * ((1/2) + (1/16)))
@@ -90,30 +118,21 @@ def generateRandomEllipse(cell_size, color):
                     cell_size * ((1/3) - (1/12)),
                     cell_size * ((1/3) + (1/12)))
 
+
   # Draw the ellipse and set its color
   cell_img = np.zeros((cell_size, cell_size, 3))
-  rr, cc = skimage.draw.ellipse(cx, cy, r_major, r_minor)
+  rr, cc = skimage.draw.ellipse(cx, cy, r_major, r_minor, shape=cell_img.shape[:2])
   cell_img[rr,cc,:] = color
 
   return cell_img
 
-# TODO: This should generate a random shade of red
-def generateRandomRed():
-  ''' Generates a random shade of red '''
-  color = np.clip(np.array([1., 0., 0.]) + npr.normal(0, .15, 3), 0, 1)
-  return 'red', color
-
-# TODO: This should generate a random shade of green
-def generateRandomGreen():
-  ''' Generates a random shade of green '''
-  color = np.clip(np.array([0., 1., 0.]) + npr.normal(0, .15, 3), 0, 1)
-  return 'green', color
-
-# TODO: This should generate a random shade of blue
-def generateRandomBlue():
-  ''' Generates a random shade of blue '''
-  color = np.clip(np.array([0., 0., 1.]) + npr.normal(0, .15, 3), 0, 1)
-  return 'blue', color
+def generateRandomColor(noise_mean=0.15, noise_var=3):
+  ''' Generates a random color'''
+  colors = {'red': np.array([1., 0., 0.]), 'green': np.array([0., 1., 0.]),
+            'blue': np.array([0., 0., 1.0])}
+  color_key, color = random.choice(list(colors.items()))
+  color = np.clip(color + npr.normal(0, noise_mean, noise_var), 0, 1)
+  return color_key, color
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -123,6 +142,8 @@ if __name__ == '__main__':
       help='Size of the grid (default=2x2)')
   parser.add_argument('--sample_size', type=int, default=64,
       help='Sample size in pixels (64x64)')
+  parser.add_argument('--aa', default=False, action='store_true',
+      help='Use anti aliasing when generating shapes')
   parser.add_argument('--debug', default=False, action='store_true',
       help='Enter debugging mode')
 
