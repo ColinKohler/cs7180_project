628bca81f1c416b0da6b5d3a853ac3c219b1114f
ColinKohler <colink78@gmail.com>
2019-02-19 18:16:19 -0500

    Added simple script to rebalence dataset. Needs refactoring with sample generation refactor.

---
+++ datasets/simple_shapes/filter_dataset.py
@@ -0,0 +1,24 @@
+import numpy as np
+import numpy.random as npr
+
+def main():
+  samples = np.load('samples.npy')
+  queries = np.load('queries.npy')
+  labels = np.load('labels.npy')
+
+  true_relation = list(); true_exists = list()
+  for true_index in np.where(labels)[0]:
+    if 'above' in queries[true_index] or 'below' in queries[true_index] or 'left' in queries[true_index] or 'right' in queries[true_index]:
+      true_relation.append(true_index)
+    else:
+      true_exists.append(true_index)
+
+  exists_subset = npr.choice(true_exists, len(true_relation))
+  false_subset = npr.choice(np.where(np.logical_not(labels))[0], len(true_relation))
+
+  np.save('rebalenced_samples.npy', samples[np.concatenate((true_relation, exists_subset, false_subset))])
+  np.save('rebalenced_queries.npy', queries[np.concatenate((true_relation, exists_subset, false_subset))])
+  np.save('rebalenced_labels.npy', labels[np.concatenate((true_relation, exists_subset, false_subset))])
+
+if __name__ == '__main__':
+  main()
