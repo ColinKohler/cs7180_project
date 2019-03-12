802cdcedb433ff243682b1da29aee876e2a59d0a
Robin Walters <rsfwalters@gmail.com>
2019-03-12 15:57:57 -0400

    fix spelling error

--- datasets/scalable_shapes/filter_dataset.py
+++ datasets/scalable_shapes/filter_dataset.py
@@ -16,9 +16,9 @@ def main():
   exists_subset = npr.choice(true_exists, len(true_relation))
   false_subset = npr.choice(np.where(np.logical_not(labels))[0], len(true_relation))
 
-  np.save('data/v2/rebalenced_samples.npy', samples[np.concatenate((true_relation, exists_subset, false_subset))])
-  np.save('data/v2/rebalenced_queries.npy', queries[np.concatenate((true_relation, exists_subset, false_subset))])
-  np.save('data/v2/rebalenced_labels.npy', labels[np.concatenate((true_relation, exists_subset, false_subset))])
+  np.save('data/v2/rebalanced_samples.npy', samples[np.concatenate((true_relation, exists_subset, false_subset))])
+  np.save('data/v2/rebalanced_queries.npy', queries[np.concatenate((true_relation, exists_subset, false_subset))])
+  np.save('data/v2/rebalanced_labels.npy', labels[np.concatenate((true_relation, exists_subset, false_subset))])
 
 if __name__ == '__main__':
   main()
