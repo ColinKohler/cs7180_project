943279d2dcbb645c6aec520f96e25797284289e3
Robin Walters <rsfwalters@gmail.com>
2019-03-12 18:05:52 -0400

    Image tensors correct shape

--- dataset_loader.py
+++ dataset_loader.py
@@ -43,6 +43,10 @@ def createScalableShapesDataLoader(dataset, batch_size=64, rebalanced=True):
     queries = np.load(os.path.join(SCALABLE_SHAPES_PATH, dataset, 'queries.npy'))
     samples = np.load(os.path.join(SCALABLE_SHAPES_PATH, dataset, 'samples.npy'))
 
+  # Image tensors for pyTorch are Batch X Channels X X_dim X Y_dim.
+  # NP images are currently Batch X X_dim X Y_dim X Channels
+  samples = np.transpose(samples,(0,3,1,2))
+
   # Create the query language
   query_lang = QueryLang()
   encoded_queries = list()
