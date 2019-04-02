5b759ebdd4ca5e46429c2445d84c50f3f1be4764
ColinKohler <colink78@gmail.com>
2019-04-02 16:50:37 -0400

    Added a visualizer to make debugging better. Some other random changes to things

--- modules.py
+++ modules.py
@@ -10,24 +10,27 @@ class And(nn.Module):
   def __init__(self):
     super(And, self).__init__()
     self.num_attention_maps = 2
+    self.name = 'And'
 
   def forward(self, attention):
     # Soft-logical and (Min)
-    return F.relu(torch.min(attention, dim=1)[0])
+    return torch.min(attention, dim=1)[0]
 
 class Or(nn.Module):
   def __init__(self):
     super(Or, self).__init__()
     self.num_attention_maps = 2
+    self.name = 'Or'
 
   def forward(self, attention):
      # Soft-logical or (Max)
-    return F.relu(torch.max(attention, dim=1)[0])
+    return torch.max(attention, dim=1)[0]
 
 class Id(nn.Module):
   def __init__(self):
     super(Id, self).__init__()
     self.num_attention_maps = 1
+    self.name = 'Id'
 
   def forward(self, input):
     return F.relu(input)
@@ -37,52 +40,54 @@ class Id(nn.Module):
 ###########################################################################################################################################
 
 class Find(nn.Module):
-  def __init__(self, context_size, num_kernels=64, text_dim=256):
+  def __init__(self, context_dim, map_dim=64, text_dim=256):
     super(Find, self).__init__()
     self.num_attention_maps = 0
+    self.name = 'Find'
 
-    self.context_size = context_size
-    self.num_kernels = num_kernels
+    self.context_dim = context_dim
+    self.map_dim = map_dim
     self.kernel_size = 1
     self.text_dim = text_dim
 
     # conv2(conv1(xvis), W*xtxt)
-    self.fc1 = nn.Linear(self.text_dim, self.num_kernels)
-    self.conv1 = nn.Conv2d(self.context_size[0], self.num_kernels, self.kernel_size)
-    self.conv2 = nn.Conv2d(self.num_kernels, 1, self.kernel_size)
+    self.fc1 = nn.Linear(self.text_dim, self.map_dim)
+    self.conv1 = nn.Conv2d(self.context_dim[0], self.map_dim, self.kernel_size)
+    self.conv2 = nn.Conv2d(self.map_dim, 1, self.kernel_size)
 
   def forward(self, context, text):
     batch_size = context.size(0)
-    text_mapped = F.relu(self.fc1(text.view(batch_size, -1))).view(batch_size, self.num_kernels, 1, 1)
-    context_mapped = F.relu(self.conv1(context))
-    eltwise_mult = F.normalize(text_mapped  * context_mapped)
-    return F.sigmoid(self.conv2(eltwise_mult))
+    text_mapped = self.fc1(text.view(batch_size, -1)).view(batch_size, self.map_dim, 1, 1)
+    context_mapped = self.conv1(context)
+    eltwise_mult = F.normalize(torch.einsum('bmxx,bmhw->bmhw', text_mapped, context_mapped), dim=1)
+    return self.conv2(eltwise_mult)
 
 class Relocate(nn.Module):
-  def __init__(self, context_size, num_kernels=64, text_dim=256):
+  def __init__(self, context_dim, map_dim=64, text_dim=256):
     super(Relocate, self).__init__()
     self.num_attention_maps = 1
+    self.name = 'Relocate'
 
-    self.context_size = context_size
-    self.num_kernels = num_kernels
+    self.context_dim = context_dim
+    self.map_dim = map_dim
     self.kernel_size = 1
     self.text_dim = text_dim
 
     # conv2(conv1(xvis) * W1*sum(a * xvis) * W2*xtxt)
-    self.fc1 = nn.Linear(self.text_dim, self.num_kernels)
-    self.fc2 = nn.Linear(self.context_size[0], self.num_kernels)
-    self.conv1 = nn.Conv2d(self.context_size[0], self.num_kernels, 1)
-    self.conv2 = nn.Conv2d(self.num_kernels, 1, 1)
+    self.fc1 = nn.Linear(self.text_dim, self.map_dim)
+    self.fc2 = nn.Linear(self.context_dim[0], self.map_dim)
+    self.conv1 = nn.Conv2d(self.context_dim[0], self.map_dim, 1)
+    self.conv2 = nn.Conv2d(self.map_dim, 1, 1)
 
   def forward(self, attention, context, text):
     batch_size = attention.shape[0]
-    text_mapped = F.relu(self.fc1(text.view(batch_size, -1)).view(batch_size, self.num_kernels, 1, 1))
+    text_mapped = F.relu(self.fc1(text.view(batch_size, -1)).view(batch_size, self.map_dim, 1, 1))
     context_mapped = F.relu(self.conv1(context))
 
     attention_softmax = F.softmax(attention.view(batch_size, -1), dim=1)
-    attention_softmax = attention_softmax.view(batch_size, 1, self.context_size[1], self.context_size[2])
+    attention_softmax = attention_softmax.view(batch_size, 1, self.context_dim[1], self.context_dim[2])
     attention = torch.sum(context * attention_softmax, dim=[2,3])
-    attention_mapped = F.sigmoid(self.fc2(attention)).view(batch_size, self.num_kernels, 1, 1)
+    attention_mapped = F.sigmoid(self.fc2(attention)).view(batch_size, self.map_dim, 1, 1)
 
     eltwise_mult = F.normalize(context_mapped * text_mapped * attention_mapped)
     return F.sigmoid(self.conv2(eltwise_mult))
@@ -92,14 +97,15 @@ class Relocate(nn.Module):
 ###########################################################################################################################################
 
 class Exist(nn.Module):
-  def __init__(self, input_size):
+  def __init__(self, input_dim):
     super(Exist, self).__init__()
     self.num_attention_maps = 1
+    self.name = 'Exist'
 
-    self.input_size = input_size
+    self.input_dim = input_dim
 
     # W * vec(a)
-    self.fc1 = nn.Linear(input_size[-1]**2, 2)
+    self.fc1 = nn.Linear(input_dim[-1]**2, 2)
 
   def forward(self, attention):
     batch_size = attention.size(0)
--- rnmn.py
+++ rnmn.py
@@ -2,48 +2,55 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import ipdb
 
+from visualizer import Visualizer
 from modules import And, Or, Id, Find, Relocate, Exist
 from encoders import QueryEncoder, ContextEncoder
 from decoders import Decoder
 
 class RNMN(nn.Module):
-  def __init__(self, query_size, embed_size, num_layers, map_dim, hidden_size, max_query_len, device, mt_norm=1, comp_length=5, comp_stop_type=1):
+  def __init__(self, query_lang, embed_dim, num_layers, map_dim, lstm_hidden_dim, text_dim, device, mt_norm=1, comp_length=5, comp_stop_type=1):
     super(RNMN, self).__init__()
     self.device = device
-    self.query_size = query_size
-    self.embed_size = embed_size
-    self.hidden_size = hidden_size
+    self.query_dim = query_lang.num_words
+    self.embed_dim = embed_dim
+    self.lstm_hidden_dim = lstm_hidden_dim
+    self.text_dim = text_dim
     self.num_layers = num_layers
     self.map_dim = map_dim
-    self.context_size = [64, 6, 6]
+    self.context_dim = [64, 6, 6]
     self.comp_length = comp_length
     self.comp_stop_type = comp_stop_type
-    self.max_query_len = max_query_len
+    self.max_query_len = query_lang.max_len
+    self.query_lang = query_lang
 
     # Create attention and answer modules
-    self.find = Find(self.context_size, num_kernels=map_dim, text_dim=1)
-    self.relocate = Relocate(self.context_size, num_kernels=map_dim, text_dim=1)
-    self.exist = Exist(self.context_size)
-    self.attention_modules = [And(), self.find, self.relocate]
+    self.find = Find(self.context_dim, map_dim=self.map_dim, text_dim=self.text_dim)
+    self.relocate = Relocate(self.context_dim, map_dim=self.map_dim, text_dim=self.text_dim)
+    self.exist = Exist(self.context_dim)
+
+    self.attention_modules = [And(), self.find]# , self.relocate]
     self.num_att_modules = len(self.attention_modules)
     self.answer_modules = [self.exist]
     [module.to(self.device) for module in self.attention_modules + self.answer_modules]
 
-    self.query_combine = nn.Linear(self.max_query_len*self.embed_size, 1)
-
     # Create query and context encoders
-    self.query_encoder = QueryEncoder(self.query_size, self.embed_size, self.hidden_size, self.device, num_layers=self.num_layers, max_query_len=self.max_query_len)
+    self.query_encoder = QueryEncoder(self.query_dim, self.embed_dim, self.lstm_hidden_dim, self.device, num_layers=self.num_layers, max_query_len=self.max_query_len)
+    self.query_combine = nn.Linear(self.max_query_len*self.embed_dim, self.text_dim)
     self.context_encoder = ContextEncoder()
 
     # Create decoder
-    self.M_size = (self.num_att_modules, sum([m.num_attention_maps for m in self.attention_modules + self.answer_modules]))
-    self.decoder = Decoder(self.max_query_len, self.hidden_size, self.M_size, self.device, num_layers=self.num_layers, mt_norm=mt_norm)
+    self.M_dim = (self.num_att_modules, sum([m.num_attention_maps for m in self.attention_modules + self.answer_modules]))
+    self.decoder = Decoder(self.max_query_len, self.lstm_hidden_dim, self.M_dim, self.device, num_layers=self.num_layers, mt_norm=mt_norm)
 
-  def forward(self, query, query_len, context, debug=False):
+    # Create visualizer
+    self.visualizer = Visualizer(self.query_lang, self.attention_modules, self.answer_modules, self.comp_length)
+
+  def forward(self, query, query_len, context, debug=False, vis=False):
     batch_size = query.size(0)
 
     # Encode the query and context
@@ -52,32 +59,38 @@ class RNMN(nn.Module):
 
     # Loop over timesteps using modules until a threshold is met
     self.decoder.hidden = query_hidden
-    self.a_t = torch.randn((batch_size, self.M_size[1], self.context_size[1], self.context_size[2]), requires_grad=True, device=self.device)
-    self.M_t = torch.randn((batch_size, self.M_size[0], self.M_size[1]), requires_grad=True, device=self.device)
+    self.a_t = torch.zeros((batch_size, self.M_dim[1], self.context_dim[1], self.context_dim[2]), requires_grad=True, device=self.device)
+    self.M_t = torch.zeros((batch_size, self.M_dim[0], self.M_dim[1]), requires_grad=True, device=self.device)
 
     self.stop_mask = torch.zeros((batch_size, self.comp_length), device=self.device)
     self.outs = torch.zeros((batch_size, self.comp_length, 2), device=self.device)
 
     for t in range(self.comp_length):
       if debug: ipdb.set_trace()
-      self.M_t, attn, stop_bits = self.decoder(self.M_t.view(batch_size, self.M_size[0]*self.M_size[1], 1).permute(2,0,1), encoded_query, debug=debug)
+      self.M_t, attn, stop_bits = self.decoder(self.M_t.view(batch_size, self.M_dim[0]*self.M_dim[1], 1).permute(2,0,1), encoded_query, debug=debug)
       self.x_t = self.query_combine(torch.einsum('dbe,ebn->ben', attn, embedded_query).view(batch_size, -1))
-      self.a_t, out = self.forward_1t(encoded_context, debug=debug)
+      b_t, a_tp1, out = self.forward_1t(encoded_context, debug=debug)
+
+      if vis:
+        self.visualizer.visualizeTimestep(t, context, query, self.a_t, self.M_t, b_t, out)
 
       if (self.comp_stop_type == 1):
         self.stop_mask[:,t] = stop_bits.squeeze(1)
         self.outs[:,t,:] = out
+
+      self.a_t = copy.copy(a_tp1)
 
     if (self.comp_stop_type == 1):
       self.stop_mask = F.softmax(self.stop_mask,dim = 1)
       out = torch.einsum('bt,bti->bi',self.stop_mask,self.outs)
 
     if debug: ipdb.set_trace()
+    if vis: self.visualizer.saveGraph()
     return F.log_softmax(out, dim=1)
 
   def forward_1t(self, encoded_context, debug=False):
     batch_size = encoded_context.size(0)
-    b_t = torch.zeros((batch_size, self.num_att_modules, self.context_size[1], self.context_size[2]), device=self.device)
+    b_t = torch.zeros((batch_size, self.num_att_modules, self.context_dim[1], self.context_dim[2]), device=self.device)
 
     # Attention map indexs
     num_att_map_inputs = [module.num_attention_maps for module in self.attention_modules + self.answer_modules]
@@ -104,7 +117,7 @@ class RNMN(nn.Module):
 
     if debug: ipdb.set_trace()
     a_tp1 = torch.einsum('bkij,bkl->blij', b_t, self.M_t)
-    return a_tp1, out
+    return b_t, a_tp1, out
 
   def _encodeQuery(self, query, query_len, debug=False):
     batch_size = query.size(0)
@@ -123,9 +136,3 @@ class RNMN(nn.Module):
 
   def loadModel(self, load_path):
     pass
-
-  def printM(self, M, round_n=3):
-    M = M.t()
-    print('  OR,   AND,  ID,   FIND, RELOC')
-    for i, module in enumerate(['Or1', 'Or2', 'And1', 'And2', 'Id', 'Reloc', 'Exist']):
-      print('{}: {}'.format(np.round(M[i].cpu().numpy(), 3), module))
